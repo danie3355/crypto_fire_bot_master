c623fd71ce1ffbfaf75e6d70dc9f8598e8389fcd
danie3355 <peixoto223366@gmail.com>
2025-06-10 02:09:13 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+TELEGRAM_BOT_TOKEN = os.getenv("7783305563:AAGZ5fkdH662e9ISSx-Tthtw2vLYYxnlVBQ")
+TELEGRAM_CHAT_ID = os.getenv("1466428936")
 
-# Criptomoedas a monitorizar (podes mudar ou adicionar mais)
+# Criptomoedas a monitorizar 
 SYMBOLS = ["BTCUSDT", "DOGEUSDT", "ETHUSDT", "SOLUSDT"]
 
-# Verificar a cada X segundos (podes mudar, 60 = 1 min)
+# Verificar a cada 40 segundos 
 CHECK_INTERVAL = 60
