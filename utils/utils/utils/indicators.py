c96a30ba405a2a07d9d5cbcc21e98eba117e777b
danie3355 <peixoto223366@gmail.com>
2025-06-10 01:39:47 +0100

    Delete utils/utils/utils directory

--- utils/utils/utils/indicators.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import ta
-
-def calculate_indicators(df):
-    df['rsi'] = ta.momentum.RSIIndicator(close=df['close'], window=14).rsi()
-    df['ema_fast'] = ta.trend.EMAIndicator(close=df['close'], window=9).ema_indicator()
-    df['ema_slow'] = ta.trend.EMAIndicator(close=df['close'], window=21).ema_indicator()
-    return df
