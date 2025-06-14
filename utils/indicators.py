551d5039a78946c5f7402133900484eea841e999
danie3355 <peixoto223366@gmail.com>
2025-06-14 21:58:22 +0100

    Update indicators.py

--- utils/indicators.py
+++ utils/indicators.py
@@ -2,11 +2,9 @@ import pandas as pd
 import pandas_ta as ta
 
 def apply_indicators(df):
-    # Garante que os dados estão em ordem cronológica
     df = df.copy()
     df.sort_index(inplace=True)
 
-    # Calcula indicadores técnicos
     df['ema20'] = ta.ema(df['close'], length=20)
     df['ema50'] = ta.ema(df['close'], length=50)
     df['rsi'] = ta.rsi(df['close'], length=14)
