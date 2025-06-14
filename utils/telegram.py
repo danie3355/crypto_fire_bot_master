3e73f38d1d7eb3a1c78f0bdb06e5daddc97346cb
danie3355 <peixoto223366@gmail.com>
2025-06-14 22:26:36 +0100

    Update telegram.py

--- utils/telegram.py
+++ utils/telegram.py
@@ -3,5 +3,14 @@ from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
 
 def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
-    payload = {"chat_id": TELEGRAM_CHAT_ID, "text": message}
-    requests.post(url, data=payload)
+    payload = {
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": message,
+        "parse_mode": "Markdown"
+    }
+
+    try:
+        response = requests.post(url, json=payload)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(f"[ERRO] Falha ao enviar mensagem para o Telegram: {e}")
